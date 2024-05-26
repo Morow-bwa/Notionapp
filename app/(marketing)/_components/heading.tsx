652405ef266e41322b8f72b7b62276e6fca4704a
Eldar <167026313+Morow-bwa@users.noreply.github.com>
2024-05-26 19:28:08 +0300

    add sidebar

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,35 +1,48 @@
-"use client"
+"use client";
+
+import Link from "next/link";
 
 import { useConvexAuth } from "convex/react";
-import { ArrowRight } from "lucide-react"
+import { SignInButton } from "@clerk/clerk-react";
+import { ArrowRight } from "lucide-react";
 
-import { Button } from "@/components/ui/button"
-import { Spinner } from "@/components/spinner"
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 
 export const Heading = () => {
     const {  isAuthenticated, isLoading } = useConvexAuth();
 
     return (
-        <div className="max-w-3xl space-y-4">
-            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-                Your Ideas, Documents, & Plans. Unifed. Welcome to 
-                <span className="underline"> Yotion!</span>
-            </h1>
-            <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-                Yotion is the connected workspace where <br />
-                better, faster work happens.
-            </h3>
-            {isLoading && (
-                <div>
-                <Spinner size="lg"/>
-                </div>
-            )}
-            {isAuthenticated && !isLoading && (
-                <Button>
-                    Join to Yotion
-                        <ArrowRight className="h-4 w-4 ml-2"/>
-                </Button>
-            )}
-        </div>
-    )
+      <div className="max-w-3xl space-y-4">
+        <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+          Your Ideas, Documents, & Plans. Unifed. Welcome to
+          <span className="underline"> Yotion!</span>
+        </h1>
+        <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+          Yotion is the connected workspace where <br />
+          better, faster work happens.
+        </h3>
+        {isLoading && (
+          <div className="w-full flex flex-col items-center justify-center">
+            <Spinner size="lg" />
+          </div>
+        )}
+        {isAuthenticated && !isLoading && (
+          <Button asChild>
+            <Link href="/documents">
+              Enter Notion
+              <ArrowRight className="ml-2 h-4 w-4" />
+            </Link>
+          </Button>
+        )}
+        {!isAuthenticated && !isLoading && (
+          <SignInButton mode="modal">
+            <Button>
+              Get Notion
+              <ArrowRight className="ml-2 h-4 w-4" />
+            </Button>
+          </SignInButton>
+        )}
+      </div>
+    );
 }
